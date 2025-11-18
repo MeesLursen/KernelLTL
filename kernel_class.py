dfd2f4d4cb8d4f46b33e74069fc4561d1417cdf5
Mees Lursen <meeslursen@gmail.com>
2025-11-18 13:23:25 +0100

    reverted to COV kernel for learning rate test

--- kernel_class.py
+++ kernel_class.py
@@ -209,7 +209,7 @@ class LTLKernel:
             sats = self._evaluate_formula_on_traces(formula=phi,batch_size=batch_size,time_index=time_index)
             vals = torch.where(sats, 
                                torch.tensor(1.0, dtype=torch.float32, device=self.device),
-                               torch.tensor(-1.0, dtype=torch.float32, device=self.device))  # (B,)
+                               torch.tensor(0.0, dtype=torch.float32, device=self.device))  # (B,)
             F[i,:] = vals
 
         self.F = F
@@ -254,9 +254,12 @@ class LTLKernel:
 
         phi_vals = torch.where(phi_sats,
                                torch.tensor(1.0, dtype=torch.float32, device=self.device),
-                               torch.tensor(-1.0, dtype=torch.float32, device=self.device))  # (B,)
+                               torch.tensor(0.0, dtype=torch.float32, device=self.device))  # (B,)
             
-        emb = (self.F @ phi_vals) / float(N) # (m,)
+        phi_centered = phi_vals - phi_vals.mean()
+        F_centered = self.F - self.F.mean(dim=1, keepdim=True)
+
+        emb = (F_centered @ phi_centered) / float(N)
 
         if self.device == 'cuda':
             emb = emb.cpu()
@@ -285,12 +288,15 @@ class LTLKernel:
         
         phi_sats = self._evaluate_formula_on_traces(formula=formula,batch_size=batch_size,time_index=time_index)
 
-        phi_vals = torch.where(phi_sats, 
+        phi_vals = torch.where(phi_sats,
                                torch.tensor(1.0, dtype=torch.float32, device=self.device),
-                               torch.tensor(-1.0, dtype=torch.float32, device=self.device))  # (B,)
+                               torch.tensor(0.0, dtype=torch.float32, device=self.device))  # (B,)
             
-        emb = (self.F @ phi_vals) / float(N) # (m,)
-        
+        phi_centered = phi_vals - phi_vals.mean()
+        F_centered = self.F - self.F.mean(dim=1, keepdim=True)
+
+        emb = (F_centered @ phi_centered) / float(N)
+
         return emb
 
 
