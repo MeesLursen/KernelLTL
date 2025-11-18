5b53852f03bda52e56a384905797995e1e2d7a61
Mees Lursen <meeslursen@gmail.com>
2025-11-18 12:47:13 +0100

    train script for REINFORCE training run

--- train_tests.py
+++ train_tests.py
@@ -17,7 +17,7 @@ def main():
         torch.cuda.set_device(local_rank)
 
     # Hyperparameters
-    num_epochs = 2
+    num_epochs = 8
 
     learning_rate = 5e-5
 
@@ -27,13 +27,13 @@ def main():
 
     eps     = 0.01
     delta   = 1 - 0.99
-    m       = 512
+    m       = 1024
 
-    train_batch_size = 8
+    train_batch_size = 32
 
     reinforce_weight = 0.3
     reinforce_baseline_momentum = 0.9
-    reinforce_reward_clip = None
+    reinforce_reward_clip = 1
         
     # Create output directory
     output_dir = "ltl_model_outputs"
@@ -59,16 +59,16 @@ def main():
     train_dataset = LTLDataset()
     train_dataset.construct_dataset_from_kernel(
         kernel=kernel,
-        k=10000,  # adjust dataset size as needed
+        k=78000,  # adjust dataset size as needed
         p_leaf=0.45,
         max_depth=2,
         batch_size=10240
     )
     
     eval_dataset = LTLDataset()
-    eval_dataset.construct_dataset_from_kernel(
+    eval_dataset.construct_dataset_from_kernel_dedupe(
         kernel=kernel,
-        k=500,  # smaller validation set
+        k=3000,  # smaller validation set
         p_leaf=0.45,
         max_depth=2,
         batch_size=10240
@@ -98,7 +98,7 @@ def main():
         warmup_steps=math.ceil((len(train_dataset) / train_batch_size) * 0.2),
         weight_decay=0.01,
         logging_dir=f"{output_dir}/logs",
-        logging_steps=20,
+        logging_steps=500,
         eval_strategy="steps",
         save_strategy="steps",
         save_steps=0.2,
