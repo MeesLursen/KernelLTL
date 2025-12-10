32092da3dd2adeba21cbfbab400d22e645cf8b88
Mees Lursen <meeslursen@gmail.com>
2025-12-10 15:50:26 +0100

    argument misnaming fix

--- scripts/curriculum_train.py
+++ scripts/curriculum_train.py
@@ -137,7 +137,7 @@ def _load_training_args(args: argparse.Namespace) -> TrainingArguments:
             "logging_strategy": "steps",
             "logging_steps": 0.02,
             "logging_dir": os.path.join(args.output_dir, "logs"),
-            "evaluation_strategy": "steps",
+            "eval_strategy": "steps",
             "eval_steps": 0.02,
             "save_strategy": "steps",
             "save_steps": 0.2,
